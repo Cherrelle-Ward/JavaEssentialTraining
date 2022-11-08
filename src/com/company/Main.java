cf4fa1bf9a7e5c55372cdafbd1b06818c7ba9482
cherrelle <cherrelle_ward@hotmail.co.uk>
2022-11-08 14:37:29 +0000

    error handling added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var sc = new Scanner(System.in);
+        try (var sc = new Scanner(System.in)) {
 
         System.out.print("Enter a numeric value: ");
         var d1 = sc.nextDouble();
@@ -14,10 +15,47 @@ public class Main {
         System.out.print("Enter a numeric value: ");
         var d2 = sc.nextDouble();
 
-        double result = d1 / d2;
+        System.out.print("+ - / * ");
+        String d3 = sc.next();
 
-        System.out.println("The answer is " + result);
+
+        switch(d3){
+            case "+":
+            double resultPlus = d1 + d2;
+            System.out.println("The answer is " + resultPlus);
+            break;
+            case "-":
+            double resultMinus = d1 - d2;
+            System.out.println("The answer is " + resultMinus);
+            break;
+            case "*":
+            double resultMultiple = d1 * d2;
+            System.out.println("The answer is " + resultMultiple);
+            break;
+            case "/":
+            double resultDivide = d1 / d2;
+            System.out.println("The answer is " + resultDivide);
+            break;
+
+        }
+    
+            
+        }  catch(InputMismatchException e){
+            e.printStackTrace();
+            System.out.println("token does not match the Float regular expression!!! " + e.getMessage());
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            System.out.println("I dont know what happened");
+        }
+
+        
+        };
+
+       
+
+
     }
 
-}
+
 
